f52646958f8ffe7eb2625b0ef0d01bba468f31bb
iLikeToCode <108116949+iLikeToCode@users.noreply.github.com>
2025-11-15 19:44:44 +0000

    prettify pages_build.py

--- pages_build.py
+++ pages_build.py
@@ -1,12 +1,3 @@
-# builds the pages after the first part of build.py has run.
-# TODO : Isa get templates setup 
-# templates/gallery.html.j2 #put the gallery html you have in here
-# templates/game.html.j2 # put the game page code in here
-# existing assests to go in the css amd scripts to copy into build later
-
-# The following TODO items are straight out of the diagram
-
-
 import os
 import json
 
@@ -18,27 +9,19 @@ with open('build/metadata.json', 'r',  encoding='utf-8') as file:
 from jinja2 import Environment, FileSystemLoader
 env = Environment(loader = FileSystemLoader('templates'))
 
-
-gallerytemp = env.get_template('gallery.html.j2')
-gametemp = env.get_template('game.html.j2')
+gallery = env.get_template('gallery.html.j2')
+game = env.get_template('game.html.j2')
 for gameFilename in data:
     gameName = gameFilename.replace('.js', '')
-    outpath = os.path.join('build', 'games', gameName, 'index.html')
-    os.makedirs(os.path.dirname(outpath), exist_ok=True)
-    print(f'Built page for {gameName} at {outpath}')
+    outPath = os.path.join('build', 'games', gameName, 'index.html')
+    os.makedirs(os.path.dirname(outPath), exist_ok=True)
+    print(f'Built page for {gameName} at {outPath}')
     
-    rendr = gametemp.render(slug=gameName, game=data[gameFilename])
-    with open(outpath, 'w', encoding='utf-8') as outfile:
-        outfile.write(rendr)
+    render = game.render(slug=gameName, game=data[gameFilename])
+    with open(outPath, 'w', encoding='utf-8') as outFile:
+        outFile.write(render)
 
-
-
-
-
-
-galleryhtml = gallerytemp.render(games=data)
-with open(os.path.join('build', 'index.html'), 'w', encoding='utf-8') as outfile:
-    outfile.write(galleryhtml)
-
-    
-
+render = gallery.render(games=data)
+with open(os.path.join('build', 'index.html'), 'w', encoding='utf-8') as outFile:
+    outFile.write(render)
+print('Built gallery page at build/index.html')
